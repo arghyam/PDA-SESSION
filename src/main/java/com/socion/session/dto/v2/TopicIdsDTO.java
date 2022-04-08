0d08e88fec428e100d818ce68004869fb60462fc
vky25 <93203895+vky25@users.noreply.github.com>
2022-04-08 18:05:44 +0530

    comments updated in topicsldsDTO file

--- src/main/java/com/socion/session/dto/v2/TopicIdsDTO.java
+++ src/main/java/com/socion/session/dto/v2/TopicIdsDTO.java
@@ -4,6 +4,7 @@ import java.math.BigInteger;
 import java.util.List;
 
 public class TopicIdsDTO {
+    // This change had to be done since the API using the DTO was broken and this is part of the fix.
     List<Long> topicIds;
 
     public List<Long> getTopicIds() {
